68d8aa1a88401c6a125a960c8dd1d9d34a73bef2
lutfiHidayanto <hajidlutfi@gmail.com>
2024-04-30 23:15:24 +0700

    regex ui edit

--- nfadfa/forms.py
+++ nfadfa/forms.py
@@ -8,4 +8,4 @@ class FiniteAutomatonForm(forms.Form):
     transitions = forms.CharField(label='Transitions', widget=forms.Textarea(attrs={'rows': 5, 'cols': 50, 'placeholder': 'q0 0 q1\nq2 1 q1'}))
 
 class RegexForm(forms.Form):
-    regex = forms.CharField(label='Regular Expression', widget=forms.TextInput(attrs={'placeholder': 'Enter regex'}))
+    regex = forms.CharField(label='Regular Expression', widget=forms.TextInput(attrs={'placeholder': 'Please Enter The Correct Regular Expression... (Example : ab(a+b)*)'}))
